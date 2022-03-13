5f98a638b146465a3cade58f0d0ae6653ce41b28
George <georgesucuzhanay100@gmail.com>
2022-03-13 18:48:37 -0400

    mock coding interview with volunteer at The Knowledge House

---
+++ practice/index.js
@@ -0,0 +1,49 @@
+// creating a function
+// params @ int
+// return the conversion form int (mins) => sec
+
+// 60 sec in 1 min => 1 * 60 => sec
+
+let example1 = 5;
+let example2 = 2;
+let example3 = 1;
+
+
+function convertToSeconds(mins){
+    let result = mins * 60;
+    // console.log(result)
+    return result;
+}
+console.log(convertToSeconds(example1))
+
+// return the first element of an array
+let arr = [1,2,3,4,5,6]
+
+function firstElementInArray(arr){
+    let firstElement = arr[0];
+    return firstElement;
+}
+
+// basic assignment
+// "mat" in result
+// concatentate "Orange"
+
+// "Apple"  => "AppleOrange"
+
+function concatenteWords(word){
+    return word + "Orange";
+}
+
+// 2 array of 
+//  1. people name []
+//  2. ocupation's []
+//  3. object that correct both name and occupation
+
+// let result = 
+
+function newObject(arr1, arr2){
+    let resultArr = []
+    for(let i = 0; i < arr1.length(); i++){
+        resultArr.append()
+    }
+}
